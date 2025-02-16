43efb11effffd28b61fd55af5136bee11ce897d4
DasTapan <tapands.das@gmail.com>
2025-02-16 21:43:04 +0530

    Write test for model

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true when published_at is nil" do
+    assert blog_posts(:drafted).draft?
+  end
+
+  test "draft? returns false where published_at is in past" do
+    refute blog_posts(:published).draft?
+  end
+
+  test "draft? returns false where published_at is in future" do
+    refute blog_posts(:scheduled).draft?
+  end
+
+
+
+  test "published? returns true where published_at is nil" do
+    assert blog_posts(:published).published?
+  end
+
+  test "published? returns false where published_at is nil" do
+    refute blog_posts(:drafted).published?
+  end
+
+  test "published? returns false where published_at is in future" do
+    refute blog_posts(:scheduled).published?
+  end
+
+
+
+  test "scheduled? returns true where published_at is in future" do
+    assert blog_posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? returns false where published_at is in past" do
+    refute blog_posts(:published).scheduled?
+  end
+
+  test "scheduled? returns false where published_at is in nil" do
+    refute blog_posts(:drafted).scheduled?
+  end
 end
