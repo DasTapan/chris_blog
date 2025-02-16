71bfbf13f23645afe316c9a4b8c1d3e5f992dd00
DasTapan <tapands.das@gmail.com>
2025-02-16 21:33:44 +0530

    Add a post status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_15_154910) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_16_105014) do
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
